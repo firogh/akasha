d9be1a26a01903dc6ff182900135b05348bae0cc
Firo Yang <firo@gmail.com>
2017-06-14 17:56:21 +0800

    Update website system

--- blog/deploy.sh
+++ blog/deploy.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 msg="Update site `date`"
 if [[ "$1" == "-b" ]]
 then
 	cd /home/firo/blog
-	cp -r ./archetypes ./config.toml ./deploy.sh ./layouts ./README.md ./static /home/firo/e/akasha/blog
+	mkdir -p /home/firo/e/blog/
+	rm -rf /home/firo/e/blog/*
+	echo -e "\033[0;32mBackup blog...\033[0m"
+	cp -r ./archetypes ./config.toml ./deploy.sh ./layouts ./README.md ./static /home/firo/e/blog
 	exit
 elif [[ "$#" -ne "0" ]]
 then
 	msg="$@"
 	echo $msg
 fi
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project. 
 hugo # if using a theme, replace by `hugo -t <yourtheme>`
 
